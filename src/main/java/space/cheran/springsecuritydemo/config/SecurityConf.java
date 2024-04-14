7f9e3f867d99cf7415801d756e12c3cdd1da2feb
Andrei Chernyshev <orsk1@yandex.ru>
2024-04-14 20:44:40 +0500

    Basic Security. User admin added.

---
+++ src/main/java/space/cheran/springsecuritydemo/config/SecurityConf.java
@@ -0,0 +1,27 @@
+package space.cheran.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConf {
+    @Bean
+    protected UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("admin"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+    @Bean
+    protected PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+}
